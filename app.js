719af6c8fbfd5fb83dfba05bfe22dde474d49699
Parjanya Shakya <parjanya57@gmail.com>
2022-04-04 18:15:38 +0545

    Updated API Routes

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+const {products} = require('./data')
 
-app.get('/',(req,res)=>{
-    res.send('Home Page')
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product
+        return {id,name,image}
+    })
+    res.json(newProducts)
 })
 
-app.get('/about',(req,res)=>{
-    res.send('About Page')
+app.get('/',(req,res)=>{
+    res.send('<h1>HomePage</h1> <a href="/api/products">Products</a>')
 })
 
 app.listen(5000,()=>{
-    console.log('Server is Listening on port 5000')
-})
-app.get('/test',()=>{
-    console.log("ere");
-})
-
-
-app.all('*',(req,res)=>{
-    res.status(404).send('<h1>Resource Not Found</h1>')
+    console.log("Server is Listening")
 })
